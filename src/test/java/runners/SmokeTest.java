05be23d77ad1e1cf6e22a05cfd6d124c9e9ebb87
WoloxAmquinte <anamaria.quintero@wolox.co>
2021-02-04 16:48:41 -0500

    configure test built block in build.gradle

--- src/test/java/runners/SmokeTest.java
+++ src/test/java/runners/SmokeTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features/jenkinstest.feature"
         , plugin = {"pretty", "html:target/cucumber", "json:target/cucumberJenkins.json"}
-        , tags = "@Jenkins"
+        , tags = "@Smoke"
         , glue = "")
-public class JenkinsTest {
+public class SmokeTest {
 }
--- src/test/resources/features/jenkinstest.feature
+++ src/test/resources/features/jenkinstest.feature
@@ -7,7 +7,7 @@ Feature: Jenkins
     Then the information on the page contains the title "Conduit"
 
   @Smoke
-  Scenario: Successful registration of a new user
+  Scenario: Failure registration of a new user
     Given that I enter the conduit site
     When I get information from the page
     Then the information on the page contains the title "Conduit2"
